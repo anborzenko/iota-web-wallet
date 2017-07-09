9c6887106fc335c7f5edc3b2dd199124a820c828
Daniel Lind <daniel.lind@fjordkraft.no>
2017-07-09 15:03:54 +0200

    Fixed js error

--- app/assets/javascripts/utils/navigation.js
+++ app/assets/javascripts/utils/navigation.js
@@ -2,9 +2,10 @@
  * Created by Daniel on 02.07.2017.
  */
 
-function redirect_to(location, keep_options=true){
+function redirect_to(location, keep_options){
+    keep_options = keep_options || true;
     if(keep_options && document.location.href.indexOf('?') !== -1){
-        location += '?' +  document.location.href.split('?')[1];
+        location += '?' + document.location.href.split('?')[1];
     }
     document.location.href = location;
 }
